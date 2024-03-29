41b43b18536e9f9922749355066c9809c7e13dbc
neekit95 <nikita-ribalkin@mail.ru>
2024-03-29 18:38:17 +0300

    add redux

--- src/components/todo-list/todo/todo.jsx
+++ src/components/todo-list/todo/todo.jsx
@@ -2,29 +2,32 @@ import React from 'react';
 import style from './todo.module.scss';
 import {useState} from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
+
 const Todo = (props) => {
 
 	const [done, setDone] = useState(false);
 	const [checked, setChecked] = useState(false);
+
 	function handleClick() {
 		setDone(!done);
 		setChecked(!checked);
 	}
 
-	function deleteTodo () {
+	function deleteTodo() {
+		console.log(props.id);
 		props.deleteTodo(props.id);
 	}
 
 	return (
 		<div
-			className={ done ? style.container2 : style.container}
+			className={done ? style.container2 : style.container}
 			onClick={handleClick}
 		>
 			<div className={style.counter}>
 				{props.count}.
 			</div>
 
-			<p className={ done ? style.text2 : style.text}>
+			<p className={done ? style.text2 : style.text}>
 				{props.text}
 			</p>
 
@@ -32,7 +35,7 @@ const Todo = (props) => {
 				className={style.button}
 				onClick={deleteTodo}
 			>
-				<DeleteIcon />
+				<DeleteIcon/>
 			</div>
 		</div>
 
